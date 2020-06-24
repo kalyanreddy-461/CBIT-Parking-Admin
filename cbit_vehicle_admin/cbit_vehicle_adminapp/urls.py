c1ca07cadaca3c39226e3b05a81ebc6b516a76c2
kalyanreddy-461 <gonekalyanreddy0805@gmail.com>
2020-06-24 19:42:02 +0530

    making an api call for managing fines

--- cbit_vehicle_admin/cbit_vehicle_adminapp/urls.py
+++ cbit_vehicle_admin/cbit_vehicle_adminapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('register',views.register,name='register'),
     path('dismiss',views.dismiss,name='dismiss'),
     path('fines',views.fines,name='fines'),
+    path('fine',views.fine,name='fine'),
     path('regstatistics',views.regstatistics,name='regstatistics'),
     path('paystatistics',views.paystatistics,name='paystatistics')
 ]
--- cbit_vehicle_admin/cbit_vehicle_adminapp/views.py
+++ cbit_vehicle_admin/cbit_vehicle_adminapp/views.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 from django.views.decorators.cache import cache_control
 from django.db import models
 import requests
-
+global token1
 # Create your views here.
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def base(request):
@@ -13,15 +13,19 @@ def base(request):
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def login(request):
     if request.method=='POST':
+        global token1
         adminid=request.POST['adminid']
         password=request.POST['password']
-        x1="cbit123123"
-        y1="cbit123123"
-        if adminid==x1 and password ==y1:
-            return redirect('register')
-        else:
-            messages.info(request,"invalid credentials")
-            return redirect('login')
+        token=requests.post(' https://cosc-vehicle.herokuapp.com/login',data={'user_id':adminid,'password':password,'usertype':'admin'})
+        print(token.json())
+        for i in token.json():
+            if 'message' in i:
+                messages.info(request,"invalid credentials")
+                return redirect('login')
+            else:
+                token1=token.json()['access_token']
+                return redirect('register')
+
     else:
         return render(request,"cbit_vehicle_adminapp/login.html")
 
@@ -32,7 +36,32 @@ def dismiss(request):
     return render(request,'cbit_vehicle_adminapp/dismiss.html')
 
 def fines(request):
+    global token1
+    date=request.GET['date']
+    date1="".join(date.split('-'))
+    print(date1)
+    url='https://cosc-vehicle.herokuapp.com/fine'
+    data = requests.get("https://cosc-vehicle.herokuapp.com/fine",
+								headers = {'Authorization':'Bearer {}'.format(token1)},
+								data = {"payment_date":date1})
+    payment=[]
+    r=data.json()
+    print(r)
+    for i in range(0,len(r)):
+        payment_details={
+            'user_id':r[i]['user_id'],
+            'fine': r[i]['fine'],
+            'date':date
+            }
+        payment.append(payment_details)
+
+    print(payment)
+    context = {'payment':payment}
+    return render(request,'cbit_vehicle_adminapp/fines.html',context)
+
+def fine(request):
     return render(request,'cbit_vehicle_adminapp/fines.html')
+
 
 def regstatistics(request):
     return render(request,'cbit_vehicle_adminapp/regstatistics.html')
