684ec5e98fde5f13dd37450b7d8ffbbc7a832653
kalyanreddy-461 <gonekalyanreddy0805@gmail.com>
2020-06-27 16:34:48 +0530

    making a put and get request api call

--- cbit_vehicle_admin/cbit_vehicle_adminapp/urls.py
+++ cbit_vehicle_admin/cbit_vehicle_adminapp/urls.py
@@ -7,10 +7,14 @@ urlpatterns = [
     path('login',views.login,name='login'),
     path('register',views.register,name='register'),
     path('dismiss',views.dismiss,name='dismiss'),
+    path('dismiss1',views.dismiss1,name='dismiss1'),
+    path('dismiss2',views.dismiss2,name='dismiss2'),
+    path('listdismiss',views.listdismiss,name='listdismiss'),
     path('fines',views.fines,name='fines'),
     path('fine',views.fine,name='fine'),
-    path('regstatistic',views.regstatistics,name='regstatistic'),
+    path('regstatistic',views.regstatistic,name='regstatistic'),
     path('paystatistic',views.paystatistic,name='paystatistic'),
     path('regstatistics',views.regstatistics,name='regstatistics'),
-    path('paystatistics',views.paystatistics,name='paystatistics')
+    path('paystatistics',views.paystatistics,name='paystatistics'),
+
 ]
--- cbit_vehicle_admin/cbit_vehicle_adminapp/views.py
+++ cbit_vehicle_admin/cbit_vehicle_adminapp/views.py
@@ -33,38 +33,75 @@ def login(request):
 def register(request):
     global token1
     url = 'https://cosc-vehicle.herokuapp.com/pending'
-
+    approve = request.GET.get('approve')
+    requests.put(url,headers = {'Authorization':'Bearer {}'.format(token1)},data={'reg_id' : approve})
 
 
     pending = requests.get(url,headers = {'Authorization':'Bearer {}'.format(token1)}).json()
-
-    context = {'pending' : pending}
-
-    return render(request,'cbit_vehicle_adminapp/register.html',context)
+    if len(pending)==0:
+        messages.info(request,"No applications are available to approve!")
+        return render(request,'cbit_vehicle_adminapp/register.html')
+    else:
+        context = {'pending' : pending}
+        return render(request,'cbit_vehicle_adminapp/register.html',context)
 
 def dismiss(request):
+    global token1
+    url = 'https://cosc-vehicle.herokuapp.com/dismiss'
+    reg_id=request.GET['reg_id']
+
+    requests.put(url,headers = {'Authorization':'Bearer {}'.format(token1)},data={'reg_id' : reg_id})
+    messages.info(request,"successfully dismissed")
+    return redirect('dismiss1')
+
+def dismiss2(request):
+    global token1
+    url = 'https://cosc-vehicle.herokuapp.com/dismiss'
+    dismiss = requests.get(url,headers = {'Authorization':'Bearer {}'.format(token1)}).json()
+    if len(dismiss)==0:
+        messages.info(request,"No dismissed vehicles are available!")
+        return redirect('listdismiss')
+    else:
+        context = {'dismiss' : dismiss}
+        return render(request,'cbit_vehicle_adminapp/listdismiss.html',context)
+
+
+def dismiss1(request):
     return render(request,'cbit_vehicle_adminapp/dismiss.html')
+
+def listdismiss(request):
+    return render(request,'cbit_vehicle_adminapp/listdismiss.html')
+
 
 def fines(request):
     global token1
+    pay = request.GET.get('pay')
+
+    requests.put("https://cosc-vehicle.herokuapp.com/fine",headers = {'Authorization':'Bearer {}'.format(token1)},data={'request_id' : pay})
+
+
     data = requests.get("https://cosc-vehicle.herokuapp.com/fine",
 								headers = {'Authorization':'Bearer {}'.format(token1)})
     payment1=[]
     r=data.json()
     print(r)
-    for i in range(0,len(r)):
-        payment_details1={
-            'user_id':r[i]['user_id'],
-            'fine': r[i]['fine'],
-            'type_of_request': r[i]['type_of_request'],
-            'payment_date': r[i]['payment_date'],
-            'request_id':r[i]['request_id'],
-            'Registration_id':r[i]['reg_id']
-            }
-        payment1.append(payment_details1)
-    print(payment1)
-    context = {'payment1':payment1}
-    return render(request,'cbit_vehicle_adminapp/fines.html',context)
+    if len(r)==0:
+        messages.info(request,"No fine payments available!")
+        return render(request,'cbit_vehicle_adminapp/fines.html')
+
+    else:
+        for i in range(0,len(r)):
+            payment_details1={
+                'user_id':r[i]['user_id'],
+                'fine': r[i]['fine'],
+                'type_of_request': r[i]['type_of_request'],
+                'payment_date': r[i]['payment_date'][0:16],
+                'request_id':r[i]['request_id'],
+                'Registration_id':r[i]['reg_id']
+                }
+            payment1.append(payment_details1)
+        context = {'payment1':payment1}
+        return render(request,'cbit_vehicle_adminapp/fines.html',context)
 
 def fine(request):
     return render(request,'cbit_vehicle_adminapp/fines.html')
@@ -72,7 +109,7 @@ def fine(request):
 def paystatistic(request):
     return render(request,'cbit_vehicle_adminapp/paystatistics.html')
 
-def regstatistics(request):
+def regstatistic(request):
     return render(request,'cbit_vehicle_adminapp/regstatistics.html')
 
 def paystatistics(request):
@@ -87,18 +124,46 @@ def paystatistics(request):
     payment=[]
     r=data.json()
     print(r)
-    for i in range(0,len(r)):
-        payment_details={
-            'user_id':r[i]['user_id'],
-            'fine': r[i]['fine'],
-            'date':date,
-            'type_of_request': r[i]['type_of_request'],
-            'request_id':r[i]['request_id'],
-            'Registration_id':r[i]['reg_id'],
-            'payment_status':r[i]['payment_status']
+    if len(r)==0:
+        messages.info(request,"No payments are available on selected date!")
+        return redirect('paystatistic')
+    for i in r:
+        if 'message' in i:
+            messages.info(request,"please select the date")
+            return redirect('paystatistic')
+        else:
+            for i in range(0,len(r)):
+                payment_details={
+                'user_id':r[i]['user_id'],
+                'fine': r[i]['fine'],
+                'date':date,
+                'type_of_request': r[i]['type_of_request'],
+                'request_id':r[i]['request_id'],
+                'Registration_id':r[i]['reg_id'],
+                'payment_status':r[i]['payment_status']
             }
-        payment.append(payment_details)
+                payment.append(payment_details)
+            context = {'payment':payment}
+            return render(request,'cbit_vehicle_adminapp/paystatistics.html',context)
 
-    print(payment)
-    context = {'payment':payment}
-    return render(request,'cbit_vehicle_adminapp/paystatistics.html',context)
+def regstatistics(request):
+    global token1
+    date=request.GET['date']
+    date1="".join(date.split('-'))
+    print(date1)
+
+    data = requests.get("https://cosc-vehicle.herokuapp.com/regstats",
+								headers = {'Authorization':'Bearer {}'.format(token1)},
+								data = {"updated_date":date1})
+    payment=[]
+    r=data.json()
+    if len(r)==0:
+        messages.info(request,"No registrations are available on selected date!")
+        return redirect('regstatistic')
+    for i in r:
+        if 'message' in i:
+            messages.info(request,"please select the date")
+            return redirect('regstatistic')
+        else:
+            context = {'r':r}
+            return render(request,'cbit_vehicle_adminapp/regstatistics.html',context)
